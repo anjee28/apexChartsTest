e35ad0fa533ead3cb0be76b30254ad7f38bd04e0
Mellhabib Angni <anjee28.twoeight@gmail.com>
2023-02-19 17:03:57 +0800

    Removed optionsLine from Chart.jsx and trasferred to data.js

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,94 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import ReactApexChart from 'react-apexcharts';
-import chartData from '../data';
+//import chartData from '../data';
+import chartDataConf from '../data';
 
 function Chart(props) {
-	let optionsLine = {
-		series: chartData.series,
-		chart: {
-			id: 'chart1',
-			height: 130,
-			type: 'area',
-			brush: {
-				target: 'chart2',
-				enabled: true,
-			},
-			selection: {
-				enabled: true,
-				xaxis: {
-					min: new Date('01 Aug 2022').getTime(),
-					max: new Date('07 Nov 2022').getTime(),
-				},
-			},
-		},
-		colors: ['#008FFB'],
-		fill: {
-			opacity: 1,
-		},
-		xaxis: {
-			type: 'datetime',
-			tooltip: {
-				enabled: false,
-			},
-		},
-		yaxis: {
-			tickAmount: 4,
-			max: 1000,
-		},
-		annotations: {
-			position: 'back',
-			yaxis: [
-				{
-					y: 250,
-					y2: null,
-					strokeDashArray: 1,
-					borderColor: '#c2c2c2',
-					fillColor: '#c2c2c2',
-					opacity: 0.45,
-					offsetX: -40,
-					offsetY: -5,
-					width: '100%',
-					yAxisIndex: 0,
-					label: {
-						borderColor: '#c2c2c2',
-						borderWidth: 1,
-						borderRadius: 2,
-						text: 'This is a test 2',
-						textAnchor: 'end',
-						position: 'right',
-						offsetX: -30,
-						offsetY: 0,
-						style: {
-							background: '#fff',
-							color: '#777',
-							fontSize: '12px',
-							fontWeight: 400,
-							fontFamily: undefined,
-							cssClass: 'apexcharts-yaxis-annotation-label',
-							padding: {
-								left: 5,
-								right: 5,
-								top: 0,
-								bottom: 2,
-							},
-						},
-					},
-				},
-			],
-		},
-	};
-
 	return (
 		<div className="brushChart">
 			<ReactApexChart
-				options={chartData.options}
-				series={chartData.series}
+				options={chartDataConf.chartData.options}
+				series={chartDataConf.chartSeries}
 				type="line"
 				height={320}
 			/>
 			<ReactApexChart
-				options={optionsLine}
-				series={optionsLine.series}
+				options={chartDataConf.optionsLine}
+				series={chartDataConf.optionsLine.series}
 				type="line"
 				height={130}
 			/>
--- src/data.js
+++ src/data.js
@@ -1,5 +1,3 @@
-import { width } from '@mui/system';
-
 const seriesData = [
 	{
 		x: '08/01/2022',
@@ -399,14 +397,14 @@ const seriesData = [
 	},
 ];
 
-const chartSeries = [
+let chartSeries = [
 	{
 		name: 'Revenue',
 		data: seriesData,
 	},
 ];
 
-const chartData = {
+let chartData = {
 	series: chartSeries,
 	options: {
 		chart: {
@@ -526,4 +524,85 @@ const chartData = {
 	},
 };
 
-export default chartData;
+let optionsLine = {
+	series: chartSeries,
+	chart: {
+		id: 'chart1',
+		height: 130,
+		type: 'area',
+		brush: {
+			target: 'chart2',
+			enabled: true,
+		},
+		selection: {
+			enabled: true,
+			xaxis: {
+				min: new Date('01 Aug 2022').getTime(),
+				max: new Date('07 Nov 2022').getTime(),
+			},
+		},
+	},
+	colors: ['#008FFB'],
+	fill: {
+		opacity: 1,
+	},
+	xaxis: {
+		type: 'datetime',
+		tooltip: {
+			enabled: false,
+		},
+	},
+	yaxis: {
+		tickAmount: 4,
+		max: 1000,
+	},
+	annotations: {
+		position: 'back',
+		yaxis: [
+			{
+				y: 250,
+				y2: null,
+				strokeDashArray: 1,
+				borderColor: '#c2c2c2',
+				fillColor: '#c2c2c2',
+				opacity: 0.45,
+				offsetX: -40,
+				offsetY: -5,
+				width: '100%',
+				yAxisIndex: 0,
+				label: {
+					borderColor: '#c2c2c2',
+					borderWidth: 1,
+					borderRadius: 2,
+					text: 'This is a test 2',
+					textAnchor: 'end',
+					position: 'right',
+					offsetX: -30,
+					offsetY: 0,
+					style: {
+						background: '#fff',
+						color: '#777',
+						fontSize: '12px',
+						fontWeight: 400,
+						fontFamily: undefined,
+						cssClass: 'apexcharts-yaxis-annotation-label',
+						padding: {
+							left: 5,
+							right: 5,
+							top: 0,
+							bottom: 2,
+						},
+					},
+				},
+			},
+		],
+	},
+};
+
+const chartDataConf = {
+	chartSeries: chartSeries,
+	chartData: chartData,
+	optionsLine: optionsLine,
+};
+
+export default chartDataConf;
